56409d62cd01406d0019afc0bfd03875182371cf
vuta1927 <35493980+vuta1927@users.noreply.github.com>
2018-06-13 17:41:18 +0700

    up

---
+++ GoogleAuth/GoogleAuth/GoogleServiceAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+
+namespace GoogleAuth
+{
+    static class GoogleServiceAccount
+    {
+        /// <summary>  
+        /// Get Access Token From JSON Key Async  
+        /// </summary>  
+        /// <param name="jsonKeyFilePath">Path to your JSON Key file</param>  
+        /// <param name="scopes">Scopes required in access token</param>  
+        /// <returns>Access token as string Task</returns>  
+        public static async Task<string> GetAccessTokenFromJSONKeyAsync(string jsonKeyFilePath, params string[] scopes)
+        {
+            using (var stream = new FileStream(jsonKeyFilePath, FileMode.Open, FileAccess.Read))
+            {
+                return await GoogleCredential
+                    .FromStream(stream) // Loads key file  
+                    .CreateScoped(scopes) // Gathers scopes requested  
+                    .UnderlyingCredential // Gets the credentials  
+                    .GetAccessTokenForRequestAsync(); // Gets the Access Token  
+            }
+        }
+
+        /// <summary>  
+        /// Get Access Token From JSON Key  
+        /// </summary>  
+        /// <param name="jsonKeyFilePath">Path to your JSON Key file</param>  
+        /// <param name="scopes">Scopes required in access token</param>  
+        /// <returns>Access token as string</returns>  
+        public static string GetAccessTokenFromJSONKey(string jsonKeyFilePath, params string[] scopes)
+        {
+            return GetAccessTokenFromJSONKeyAsync(jsonKeyFilePath, scopes).Result;
+        }
+
+    }
+}
---
+++ GoogleAuth/GoogleAuth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace GoogleAuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetTokenAndCall();
+            Console.ReadLine();
+        }
+
+
+        public static void GetTokenAndCall()
+        {
+            var token = GoogleServiceAccount.GetAccessTokenFromJSONKey(
+                "ivydrive-207109-5d7ca92ee3ff.json",
+                "https://www.googleapis.com/auth/userinfo.profile");
+            Console.WriteLine("AccessToken: {0}",token);
+            Console.WriteLine(new HttpClient().GetStringAsync($"https://www.googleapis.com/plus/v1/people/109956788316344882635?access_token={token}").Result);
+        }
+    }
+}
